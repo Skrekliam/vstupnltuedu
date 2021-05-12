8e8e64edf9b2189c6651409dc07a3fe7897fac31
Skrekliam <typnickman@gmail.com>
2021-05-12 14:44:09 +0300

    Add easy mode

--- script.js
+++ script.js
@@ -1,19 +1,49 @@
 (function () {
-    'use strict'
-  
-    // Form validation
-    var forms = document.querySelectorAll('.needs-validation')
-  
-    Array.prototype.slice.call(forms)
-      .forEach(function (form) {
-        form.addEventListener('submit', function (event) {
-          if (!form.checkValidity()) {
-            event.preventDefault()
-            event.stopPropagation()
-          }
-  
-          form.classList.add('was-validated')
-        }, false)
-      })
-  })()
+  "use strict";
 
+  // Form validation
+  var forms = document.querySelectorAll(".needs-validation");
+
+  Array.prototype.slice.call(forms).forEach(function (form) {
+    form.addEventListener(
+      "submit",
+      function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
+})();
+
+let largeSize = true;
+let textSize = 25;
+let btnVal = "Збільшити";
+const form = document.getElementsByTagName("form")[0];
+const btnCng = document.getElementById("changeSize");
+const inputs = document.getElementsByTagName("input");
+const options = document.getElementsByTagName("option");
+const submitBtn = document.getElementById("submit");
+function easyMode() {
+  if (largeSize) {
+    textSize = 25;
+    btnVal = "Зменшити";
+  } else {
+    textSize = 16;
+    btnVal = "Збільшити";
+  }
+  for (i of inputs) {
+    i.style.fontSize = textSize + "px";
+  }
+  for (i of options) {
+    i.style.fontSize = textSize + "px";
+  }
+  btnCng.textContent = btnVal + " розмір полів";
+  form.style.fontSize = textSize + "px";
+  submitBtn.style.fontSize = textSize + "px";
+  largeSize = !largeSize;
+}
